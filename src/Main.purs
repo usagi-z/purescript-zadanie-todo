70ff7cdf9251abf93c8cbddd1ca64da81733361a
usagi-z <wzajaczkowski91@gmail.com>
2022-04-05 18:39:13 +0200

    item editability (with validation)

--- src/Main.purs
+++ src/Main.purs
@@ -3,14 +3,14 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Specular.Dom.Element (attrs, dynText, el, text)
+import Specular.Dom.Element (attrs, dynText, el, text, onClick)
 import Specular.Dom.Widget
 import Specular.Dom.Widgets.Input
 import Specular.Dom.Node.Class ((:=))
 import Specular.Ref (Ref)
 import Specular.Ref as Ref
-import Specular.FRP (Dynamic, Event, filterJustEvent, withDynamic_)
-import Specular.FRP.Base (subscribeEvent_)
+import Specular.FRP (Dynamic, Event, filterJustEvent, withDynamic_, changed)
+import Specular.FRP.Base (subscribeEvent_, never)
 import Specular.Dom.Builder.Class (domEventWithSample, elDynAttr')
 import Specular.Dom.Browser as Browser
 import Unsafe.Coerce (unsafeCoerce)
@@ -41,8 +41,36 @@ todoTextInput = do
   maybeInput <- domEventWithSample (getInputOnEnterAndClear element) "keypress" element
   pure $ filterJustEvent maybeInput
 
+itemValidationMessage :: Dynamic (Maybe String) -> Widget Unit
+itemValidationMessage mMsgD = withDynamic_ mMsgD $ \maybeMessage ->
+  case maybeMessage of
+    Just msg ->
+      el "p" [attrs ("style" := "color:red")] $ text msg
+    Nothing -> pure unit
+
 listItem :: Ref String -> Widget Unit
-listItem todo = el "li" mempty $ dynText $ Ref.value todo
+listItem todo = do
+  editing <- Ref.new false
+  localValidation :: Ref (Maybe String) <- Ref.new Nothing
+  el "li" mempty $
+    withDynamic_ (Ref.value editing) $ \editingP ->
+      if editingP then
+        do todoText <- Ref.read todo
+           input <- textInput { initialValue: todoText
+                              , attributes: pure mempty
+                              , setValue: never
+                              }
+           itemValidationMessage (Ref.value localValidation)
+           inputEvt <- textInputValueEventOnEnter input
+           (flip subscribeEvent_) inputEvt $ \newTodo ->
+             if String.null newTodo
+             then Ref.write localValidation $ Just "empty input not allowed"
+             else do Ref.write todo newTodo
+                     Ref.write editing false
+                     Ref.write localValidation Nothing
+                     
+      else el "div" [onClick (\_ -> Ref.write editing true)] $
+             dynText $ Ref.value todo
 
 todosList :: Array (Ref String) -> Widget Unit
 todosList = traverse_ listItem
@@ -54,12 +82,12 @@ viewTODOs todosD =
   withDynamic_ todosD todosList
 
 maybeAppendTODO :: Ref (Array (Ref String)) -> Ref String -> String -> Effect Unit
-maybeAppendTODO todos validation new = do
-  if String.null new
+maybeAppendTODO todos validation newTodo = do
+  if String.null newTodo
     then do
       Ref.write validation "empty input not allowed"
     else do
-      newRef <- Ref.new new
+      newRef <- Ref.new newTodo
       Ref.modify todos $ (flip Array.snoc) newRef
       Ref.write validation ""
 
